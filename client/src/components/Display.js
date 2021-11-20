50d6af4076495004ca53d5ec8cd6d4abb54b8f30
Hwanhee Chae <hh@hwan.me>
2021-11-20 22:12:06 +0900

    feat: setup basic react-router-dom and components

---
+++ client/src/components/Display.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Display() {
+  return (
+    <div className="display">
+      <div class="main-display">
+          <i class="fas fa-quote-left"></i>
+          <div class="main-quote">
+            <ruby>濡<rt>ぬ</rt></ruby>れたタオルで<ruby>洗<rt>あら</rt></ruby>いざらい<ruby>吐<rt>は</rt></ruby>かせるね
+          </div>
+          <i class="fas fa-quote-right"></i>
+      </div>
+    </div>
+  )
+}
+
+export default Display;
---
+++ client/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+function Header() {
+  return (
+    <header class="control">
+      <span>[copy permalink]</span>
+      <Link to="/create">[create philosophy]</Link>
+      <span className="ml-auto">[lightmode]</span>
+    </header>
+  )
+}
+
+export default Header;
