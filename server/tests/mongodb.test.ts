21c485dd53ccd3a897a9f9574435fa2e40811b84
Hwanhee Chae <hh@hwan.me>
2021-11-24 01:22:33 +0900

    feat: add inserting mockup data when testing

--- server/tests/mongodb.test.ts
+++ server/tests/mongodb.test.ts
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 import quote from '../models/Quote';
+import mockup from './mockup.json';
 
 describe('MongoDB Functioanlity Test', () => {
   beforeAll(async () => {
@@ -7,10 +8,21 @@ describe('MongoDB Functioanlity Test', () => {
       throw new Error('No DBURI provided');
     }
     await mongoose.connect(process.env.DBURI);
+    await quote.collection.drop();
   });
 
   afterAll(async () => {
     await mongoose.connection.close();
+  });
+
+  test('collection dropped', async () => {
+    const count = await quote.countDocuments();
+    expect(count).toBe(0);
+  });
+
+  test('inserting mockup data', async () => {
+    const inserted = await quote.insertMany(mockup);
+    expect(inserted.length).toBe(mockup.length);
   });
 
   test('querying works', async () => {
